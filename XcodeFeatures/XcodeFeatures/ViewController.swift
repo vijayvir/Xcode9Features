4e57fdd8c66b52e37dfcf8ee34b13a1ddead18d7
vijay vir <virvijay37@gmail.com>
2017-07-29 16:01:58 +0530

    First
    Git hub Commit

---
+++ XcodeFeatures/XcodeFeatures/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeFeatures
+//
+//  Created by vijay vir on 7/29/17.
+//  Copyright © 2017 vijay vir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		print("First Git hub print")
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
